63a7f5999cb19cc1b6e6871803368e6959ffedd5
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-11-13 07:16:31 +0700

    update auth controller

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -75,7 +75,7 @@ const getAllUsers = async (req, res) => {
 };
 
 const logout = async (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
