bdc59e5d897db1403f79e100b73d0727c3d0a811
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-11-15 14:40:27 +0700

    update users route

--- model/users.model.js
+++ model/users.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['user', 'admin', 'super admin'],
+      enum: ['user', 'admin', 'superAdmin'],
       required: true,
       default: 'user',
     }
