253243752aac6a73593b2f20429f76a1e843bf14
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-11-20 11:11:12 +0700

    update project

--- controller/pengembalian.controller.js
+++ controller/pengembalian.controller.js
@@ -11,25 +11,100 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-async function createPengembalian(pengembalianData, photoFile) {
-    try {
-      const newPengembalian = new PengembalianModel(pengembalianData);
-      const savedPengembalian = await newPengembalian.save();
+const createPengembalian = async (req, res) => {
+  try {
+    const {
+      kondisi_aset,
+      tanggal_pengembalian,
+      lokasi,
+      id_aset,
+      username,
+    } = req.body;
 
-      const photoFileName = `pengembalian_${savedPengembalian._id}_${Date.now()}.jpg`;
-      const photoRef = storage.ref().child(photoFileName);
-      
-      const photoSnapshot = await photoRef.put(photoFile);
-      const photoURL = await photoSnapshot.ref.getDownloadURL();
+    const aset = await AsetModel.findOne({ _id: id_aset });
 
-      savedPengembalian.foto = photoURL;
-      await savedPengembalian.save();
-  
-      return savedPengembalian;
-    } catch (error) {
-      throw new Error(error.message);
+    if (!aset) {
+      return res.status(404).json({
+        error: {
+          message: "Asset not found",
+        },
+      });
     }
+
+    if (!aset.is_borrowed) {
+      return res.status(400).json({
+        error: {
+          message: "Asset is not currently borrowed",
+        },
+      });
+    }
+
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({
+        error: {
+          message: "User not found",
+        },
+      });
+    }
+
+    const existingPeminjaman = await PeminjamanModel.findOne({
+      id_aset,
+      id_user: user._id,
+      status: "Pending",
+    });
+
+    if (!existingPeminjaman) {
+      return res.status(400).json({
+        error: {
+          message: "No pending borrowing found for this asset and user",
+        },
+      });
+    }
+
+    // Handle the photo file upload
+    const photoFile = req.file; // Assuming the file is sent as 'file' in the request
+
+    // Your code for photo upload to Firebase Storage here
+    const photoFileName = `pengembalian_${existingPeminjaman._id}_${Date.now()}.jpg`;
+    const photoRef = storage.ref().child(photoFileName);
+    const photoSnapshot = await photoRef.put(photoFile.buffer); // Uploading the file buffer
+
+    const photoURL = await photoSnapshot.ref.getDownloadURL();
+
+    const newPengembalian = new PengembalianModel({
+      lokasi,
+      kondisi_aset,
+      tanggal_pengembalian,
+      status: "Pending",
+      jenis: "Pengembalian",
+      id_aset: aset._id,
+      id_user: user._id,
+      foto: photoURL,
+    });
+
+    const savedPengembalian = await newPengembalian.save();
+
+    aset.is_borrowed = false;
+    await aset.save();
+
+    existingPeminjaman.status = "Completed";
+    await existingPeminjaman.save();
+
+    res.status(201).json({
+      message: "Pengembalian berhasil dibuat",
+      pengembalian: savedPengembalian,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: {
+        message: "Error creating pengembalian",
+        details: error.message,
+      },
+    });
   }
+};
   
 
 module.exports = { createPengembalian };
