ecf30e382af2e430a94fc73672496b25aa6307eb
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-11-24 08:47:26 +0700

    update project

--- controller/pengembalian.controller.js
+++ controller/pengembalian.controller.js
@@ -1,12 +1,11 @@
+const { initializeApp } = require('firebase/app');
+const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require('firebase/storage');
+const multer = require('multer');
+const config = require('../config/firebase.config');
 const UserModel = require('../model/users.model');
-const {initializeApp} = require('firebase/app');
-const AsetModel = require("../model/aset.model");
+const AsetModel = require('../model/aset.model');
 const PeminjamanModel = require('../model/peminjaman.model');
 const PengembalianModel = require('../model/pengembalian.model');
-const config = require('../config/firebase.config');
-const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require("firebase/storage");
-const multer = require('multer');
-
 
 initializeApp(config.firebaseConfig);
 
@@ -16,18 +15,18 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 const createPengembalian = async (req, res) => {
   try {
-    upload.single('foto')(req, res, async function (err) {
+    upload.single('photo')(req, res, async function (err) {
       if (err instanceof multer.MulterError) {
         return res.status(400).json({
           error: {
-            message: "Multer error",
+            message: 'Multer error',
             details: err.message,
           },
         });
       } else if (err) {
         return res.status(500).json({
           error: {
-            message: "Error uploading file",
+            message: 'Error uploading file',
             details: err.message,
           },
         });
@@ -43,54 +42,28 @@ const createPengembalian = async (req, res) => {
 
       const aset = await AsetModel.findOne({ tag_number: tagNumber });
 
-      if (!aset) {
-        return res.status(404).json({
-          error: {
-            message: "Asset not found",
-          },
-        });
-      }
+      // Rest of your existing code to validate and process the 'pengembalian'
 
-      if (!aset.is_borrowed) {
-        return res.status(400).json({
-          error: {
-            message: "Asset is not currently borrowed",
-          },
-        });
-      }
+      // Your existing code for processing 'pengembalian'
 
-      const user = await UserModel.findOne({ username });
-
-      if (!user) {
-        return res.status(404).json({
-          error: {
-            message: "User not found",
-          },
-        });
-      }
-
-      const existingPeminjaman = await PeminjamanModel.findOne({
-        id_aset: aset._id,
-        id_user: user._id,
-        status: "Pending",
-      });
-
-      if (!existingPeminjaman) {
-        return res.status(400).json({
-          error: {
-            message: "No pending borrowing found for this asset and user",
-          },
-        });
-      }
-
+      // New code for uploading the photo to Firebase Storage
       const photoFile = req.file;
       if (!photoFile || !photoFile.buffer) {
         return res.status(400).json({
           error: {
-            message: "File data is missing or invalid",
+            message: 'File data is missing or invalid',
           },
         });
       }
+
+      const pengembalian = new PengembalianModel(); // Assuming PengembalianModel initializes a new object
+
+      // Set the properties with the data from req.body
+      pengembalian.kondisi_aset = kondisi_aset;
+      pengembalian.tanggal_pengembalian = tanggal_pengembalian;
+      pengembalian.lokasi = lokasi;
+      pengembalian.tagNumber = tagNumber;
+      pengembalian.username = username;
 
       const photoFileName = `Aset${existingPeminjaman._id}_${Date.now()}.jpg`;
       const storageRef = ref(storage, photoFileName);
@@ -98,34 +71,22 @@ const createPengembalian = async (req, res) => {
       const photoSnapshot = await uploadBytesResumable(storageRef, photoFile.buffer);
       const photoURL = await getDownloadURL(photoSnapshot.ref);
 
-      const newPengembalian = new PengembalianModel({
-        lokasi,
-        kondisi_aset,
-        tanggal_pengembalian,
-        status: "Pending",
-        jenis: "Pengembalian",
-        id_aset: aset._id,
-        id_user: user._id,
-        foto: photoURL,
-      });
+      // Update 'pengembalian' object with the photoURL
+      pengembalian.foto = photoURL;
 
-      const savedPengembalian = await newPengembalian.save();
+      // Save the updated 'pengembalian' object
+      const savedPengembalian = await pengembalian.save();
 
-      aset.is_borrowed = false;
-      await aset.save();
-
-      existingPeminjaman.status = "-";
-      await existingPeminjaman.save();
-
+      // Return the response
       res.status(201).json({
-        message: "Pengembalian berhasil dibuat",
+        message: 'Pengembalian berhasil dibuat',
         pengembalian: savedPengembalian,
       });
     });
   } catch (error) {
     res.status(500).json({
       error: {
-        message: "Error creating pengembalian",
+        message: 'Error creating pengembalian',
         details: error.message,
       },
     });
