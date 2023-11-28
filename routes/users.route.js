6f74ff60173656bd18040e9b5c4ae29438e41888
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-11-29 06:58:55 +0700

    update project

--- routes/users.route.js
+++ routes/users.route.js
@@ -4,9 +4,10 @@ const { authenticate } = require('../middleware/auth.middleware');
 const router = express.Router();
 
 router.get('/profile', authenticate, (req, res) => {
+  const id = req.user.id;
   const username = req.user.username;
   const role = req.user.role;
-  res.json({ username , role});
+  res.json({ username , role, id});
 });
 
 module.exports = router;
