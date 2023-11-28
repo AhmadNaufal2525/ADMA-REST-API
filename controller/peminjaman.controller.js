4714c4940ba5d0cde7452f79dd3a4ed1fa6bc1c1
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-11-29 06:40:19 +0700

    update project

--- controller/peminjaman.controller.js
+++ controller/peminjaman.controller.js
@@ -91,7 +91,7 @@ const acceptPeminjaman = async (req, res) => {
   try {
     const peminjamanId = req.params.id;
     const userId = req.params.id;
-    const adminId = req.body.id;
+    const adminId = req.body.userId;
     const peminjaman = await PeminjamanModel.findById(peminjamanId);
 
     if (!peminjaman) {
